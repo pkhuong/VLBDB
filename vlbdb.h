8f1ec8307a32303a70ff14cc7567807907ed9796
Paul Khuong <pvk@pvk.ca>
2011-10-21 03:24:34 -0400

    More convenience functions (format-style, mostly)

--- vlbdb.h
+++ vlbdb.h
@@ -26,11 +26,15 @@ void vlbdb_register_function_name (vlbdb_unit_t *, const char *, size_t);
 
 void vlbdb_register_block (vlbdb_unit_t *, void *, size_t);
 
-/* TODO */
+__attribute__ ((format (printf, 3, 4)))
 void * vlbdb_specializef(vlbdb_unit_t *, void * function, const char *, ...);
+__attribute__ ((format (printf, 3, 0)))
 void * vlbdb_vspecializef(vlbdb_unit_t *, void * function, const char *, va_list);
-void * vlbdb_block_specializef(vlbdb_unit_t *, void * function, const char *, ...);
-void * vlbdb_vblock_specializef(vlbdb_unit_t *, void * function, const char *, va_list);
+__attribute__ ((format (printf, 3, 4)))
+void * vlbdb_block_specializef(vlbdb_unit_t *, void * block, const char *, ...);
+__attribute__ ((format (printf, 3, 0)))
+void * vlbdb_vblock_specializef(vlbdb_unit_t *, void * block, const char *, va_list);
+void * vlbdb_block_specialize(vlbdb_unit_t *, void * block);
 
 int vlbdb_intern_range (vlbdb_unit_t *, void *, size_t);
 int vlbdb_register_range (vlbdb_unit_t *, void *, size_t);
@@ -40,5 +44,9 @@ void vlbdb_bind_int(vlbdb_binder_t *, long long);
 void vlbdb_bind_fp(vlbdb_binder_t *, double);
 void vlbdb_bind_ptr(vlbdb_binder_t *, void *);
 void vlbdb_bind_range(vlbdb_binder_t *, void *, size_t);
+__attribute__ ((format (printf, 2, 3)))
+void vlbdb_bindf(vlbdb_binder_t *, const char *, ...);
+__attribute__ ((format (printf, 2, 0)))
+void vlbdb_vbindf(vlbdb_binder_t *, const char *, va_list);
 void * vlbdb_specialize(vlbdb_binder_t *);
 #endif
